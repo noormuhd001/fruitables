4aec3953a2f5f1f0b0b2a902f8fe4bb965c9ce8d
Noor Muhammed <noor.bytekat@gmail.com>
2024-06-19 10:43:29 +0530

    created service and try catch on shop controller on userpage

--- app/Http/Controllers/User/ShopController.php
+++ app/Http/Controllers/User/ShopController.php
@@ -6,17 +6,30 @@ use App\Http\Controllers\Controller;
 use App\Models\categories;
 use App\Models\Product;
 use App\Models\Review;
+use App\Services\Shopmanagement\ShopManagementService;
+
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
     //
 
-    public function view($id){
-        $review = Review::all();
-       $categories = categories::all();
-        $product = Product::findorfail($id);
-        $allproducts = product::all();
-        return view('user.detail.index',['product'=>$product,'categories'=>$categories,'allproducts'=>$allproducts,'review'=>$review]);
+    private $shopmanagementservice;
+
+    public function __construct(ShopManagementService $shopmanagementservice)
+    {
+        $this->shopmanagementservice = $shopmanagementservice;
+    }
+
+    public function view($id)
+    {
+        try {
+            $data = $this->shopmanagementservice->getdata($id);
+            return view('user.detail.index', $data);
+        } catch (\Exception $e) {
+
+            report($e);
+            return abort(500);
+        }
     }
 }
