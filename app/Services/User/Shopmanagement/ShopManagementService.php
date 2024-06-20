6d7e8f4c24a828d4e675baa986311bad18b1c8b4
Noor Muhammed <noor.bytekat@gmail.com>
2024-06-20 17:27:36 +0530

    modified item detail page and added  featured products

--- app/Services/User/Shopmanagement/ShopManagementService.php
+++ app/Services/User/Shopmanagement/ShopManagementService.php
@@ -3,6 +3,7 @@
 namespace App\Services\User\ShopManagement;
 
 use App\Models\categories;
+use App\Models\offer;
 use App\Models\Product;
 use App\Models\Review;
 
@@ -12,12 +13,14 @@ class ShopManagementService
     $review = Review::all();
     $categories = categories::all();
     $product = Product::findOrFail($id);
+    $offerproducts = offer::all();
     $allproducts = Product::all();
 
     return [
         'product' => $product,
         'categories' => $categories,
         'allproducts' => $allproducts,
+        'offerproducts'=>$offerproducts,
         'review' => $review,
     ];
  }
